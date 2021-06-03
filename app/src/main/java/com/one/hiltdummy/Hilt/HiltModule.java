1ea4884c02293c85b3f3a544a24f0133b7ecaa9a
HarshSinghRajawat <harshsinghrajawat86@gmail.com>
2021-06-03 22:30:55 +0530

    RecycleView-Retrofit-Hilt

---
+++ app/src/main/java/com/one/hiltdummy/Hilt/HiltModule.java
@@ -0,0 +1,44 @@
+package com.one.hiltdummy.Hilt;
+
+import com.one.hiltdummy.carTestExample.Engine;
+import com.one.hiltdummy.carTestExample.Wheels;
+import com.squareup.moshi.Moshi;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.Module;
+import dagger.hilt.android.components.ApplicationComponent;
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
+@Module
+@InstallIn(ApplicationComponent.class)
+public class HiltModule {
+    String url="https://jsonplaceholder.typicode.com/";
+
+    @Provides
+    @Singleton
+    public static Wheels provideWheels(){return new Wheels();}
+    @Provides
+    @Singleton
+    public static Engine provideEngine(){return new Engine();}
+    @Singleton
+    @Provides
+    public Moshi provideMoshi(){
+        return new Moshi.Builder().build();
+    }
+
+    @Singleton
+    @Provides
+    public Retrofit provideRetrofit(Moshi converter){
+        Retrofit retrofit=new Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(MoshiConverterFactory.create(converter))
+                .build();
+        return retrofit;
+    }
+
+}
--- app/src/main/java/com/one/hiltdummy/MainActivity.java
+++ app/src/main/java/com/one/hiltdummy/MainActivity.java
@@ -1,23 +1,77 @@
 package com.one.hiltdummy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 
+import com.one.hiltdummy.Retrofit.Data;
+import com.one.hiltdummy.Retrofit.RetroNetworkInterface;
 import com.one.hiltdummy.carTestExample.Car;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
 import dagger.hilt.android.AndroidEntryPoint;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 @AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
     @Inject
     Car car;
+    @Inject
+    Retrofit retrofit;
+    private RecyclerView recyclerView;
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         car.Drive();
+
+        ProgressBar bar=findViewById(R.id.progressBar);
+
+        recyclerView=findViewById(R.id.list);
+        GridLayoutManager mGrid=new GridLayoutManager(MainActivity.this,1);
+        recyclerView.setLayoutManager(mGrid);
+        recyclerView.setHasFixedSize(true);
+
+        //((CustomApplication)getApplication()).getNetworkComponent().inject(this);
+
+        RetroNetworkInterface mService=retrofit.create(RetroNetworkInterface.class);
+        Call<List<Data>> list=mService.getToDoItems();
+
+        list.enqueue(new Callback<List<Data>>() {
+            @Override
+            public void onResponse(Call<List<Data>> call, Response<List<Data>> response) {
+                MyAdapter myAdapter=new MyAdapter(MainActivity.this,response.body());
+                recyclerView.setAdapter(myAdapter);
+                bar.setVisibility(View.GONE);
+                Log.d(TAG, response.code()+"");
+            }
+
+            @Override
+            public void onFailure(Call<List<Data>> call, Throwable t) {
+
+            }
+        });
+
+
+
+
+
+
+
+
+
     }
 }
